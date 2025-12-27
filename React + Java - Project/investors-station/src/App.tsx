34317c3570180827654aff82c74544d53484764a
miguelzvd <dilsonmiguel25@gmail.com>
2025-12-27 16:14:05 -0300

    feat: implement user form handling and display; add user interface and initial user data

--- React + Java - Project/investors-station/src/App.tsx
+++ React + Java - Project/investors-station/src/App.tsx
@@ -1,35 +1,174 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import type { User } from "./interfaces/user";
+import { USERS } from "./constants/users";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const userInfoDefaultValue: User = {
+    name: "",
+    age: 0,
+    email: "",
+    investment: "",
+  };
+
+  const [userFormInfo, setUserFormInfo] = useState<User>(userInfoDefaultValue);
+
+  const [users, setUsers] = useState<User[]>(USERS);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "2rem",
+          marginBottom: "4rem",
+          marginTop: "4rem",
+        }}
+      >
+        <form
+          style={{ background: "gray", padding: "1rem" }}
+          onSubmit={(e) => {
+            e.preventDefault();
+            setUsers((prev) => {
+              if (prev.some((user) => user.email === userFormInfo.email)) {
+                alert("O email precisa ser diferente dos que já existem");
+                return [...prev];
+              }
+
+              return [...prev, userFormInfo];
+            });
+            setUserFormInfo(userInfoDefaultValue);
+            console.log(`Usuário: ${JSON.stringify(userFormInfo)}`);
+          }}
+        >
+          <p>
+            <label htmlFor="name">Nome: </label>
+            <input
+              type="text"
+              id="name"
+              value={userFormInfo.name}
+              onChange={(e) =>
+                setUserFormInfo((prev) => ({
+                  ...prev,
+                  name: e.target.value,
+                }))
+              }
+            />
+          </p>
+          <p>
+            <label htmlFor="age">idade: </label>
+            <input
+              type="number"
+              id="age"
+              value={userFormInfo.age}
+              onChange={(e) =>
+                setUserFormInfo((prev) => ({
+                  ...prev,
+                  age: Number(e.target.value),
+                }))
+              }
+            />
+          </p>
+          <p>
+            <label htmlFor="email">email: </label>
+            <input
+              type="email"
+              id="email"
+              value={userFormInfo.email}
+              onChange={(e) =>
+                setUserFormInfo((prev) => ({
+                  ...prev,
+                  email: e.target.value,
+                }))
+              }
+            />
+          </p>
+          <p>
+            <label htmlFor="investment">investimentos: </label>
+            <input
+              type="text"
+              id="investment"
+              value={userFormInfo.investment}
+              onChange={(e) =>
+                setUserFormInfo((prev) => ({
+                  ...prev,
+                  investment: e.target.value,
+                }))
+              }
+            />
+          </p>
+
+          <input type="submit" value={"Adicionar"} />
+        </form>
+
+        {users?.map((user, index) => {
+          return (
+            <div
+              style={{
+                color: "black",
+                background: "white",
+                position: "relative",
+              }}
+              key={index}
+            >
+              <span
+                style={{
+                  display: "block",
+                  margin: "0 auto",
+                  width: "fit-content",
+                }}
+              >
+                Usuário {index + 1}
+              </span>
+
+              <button
+                onClick={() =>
+                  setUsers((prev) => {
+                    const newArr = prev.filter(
+                      (arrUser) => arrUser.email != user.email
+                    );
+                    return newArr;
+                  })
+                }
+                style={{
+                  width: "fit-content",
+                  height: "fit-content",
+                  padding: "0.2rem",
+                  position: "absolute",
+                  top: "0.5rem",
+                  right: "0.5rem",
+                }}
+              >
+                X
+              </button>
+
+              <ul
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  listStyle: "none",
+                }}
+              >
+                <li>
+                  <b>Nome:</b> {user.name}
+                </li>
+                <li>
+                  <b>Email:</b> {user.email}
+                </li>
+                <li>
+                  <b>Idade:</b> {user.age}
+                </li>
+                <li>
+                  <b>Investimentos:</b> {user.investment}
+                </li>
+              </ul>
+            </div>
+          );
+        })}
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ React + Java - Project/investors-station/src/constants/users.ts
@@ -0,0 +1,13 @@
+import type { User } from "../interfaces/user";
+
+export const USERS: User[] = [
+  { name: "John", age: 21, email: "john@gmail.com", investment: "2222" },
+  { name: "Nico", age: 21, email: "nico@gmail.com", investment: "2222" },
+  {
+    name: "Michael",
+    age: 21,
+    email: "michael@gmail.com",
+    investment: "2222",
+  },
+  { name: "Trevor", age: 21, email: "trevor@gmail.com", investment: "2222" },
+];
