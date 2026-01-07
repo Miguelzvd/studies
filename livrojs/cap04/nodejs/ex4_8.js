8abf095be928bd0d793c6695d5801725f74ab59b
miguelzvd <dilsonmiguel25@gmail.com>
2026-01-06 21:09:13 -0300

    fix: improve console log messages for parcel calculation

--- livrojs/cap04/nodejs/ex4_8.js
+++ livrojs/cap04/nodejs/ex4_8.js
@@ -7,11 +7,10 @@ const valorCompra = Number(prompt("Valor da compra R$: "));
 
 const aux = Math.floor(valorCompra / valorMinParcela);
 
-  console.log("valor parcela", aux)
-
+console.log("Quantidade minima de vezes que pode parcelar", aux);
 
 const parcelas = aux === 0 ? 1 : aux > numMaxParcelas ? numMaxParcelas : aux;
 
 const valorParcelas = valorCompra / parcelas;
 
-console.log(`Pode pagar em ${parcelas}x de R$ ${valorParcelas.toFixed(2)}`)
+console.log(`Pode pagar em ${parcelas}x de R$ ${valorParcelas.toFixed(2)}`);
